46041643420b61986cbe305b7af6c0babf6a6b00
yvessrib <yves@gec.inatel.br>
2025-03-10 08:45:49 -0300

    style: favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -53,7 +53,7 @@ export default function RootLayout({
     <head>
       <meta charSet="UTF-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <link rel="page icon" href="\logo.svg" />
+      <link rel="page icon" href="\favicon.ico" />
       {/* biome-ignore lint/style/noUnusedTemplateLiteral: <explanation> */}
       
         <script type="application/ld+json">

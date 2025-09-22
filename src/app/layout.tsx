572b734206a6fc3c4e3be0ac695fb92f4413c0cc
yvessrib <yves@gec.inatel.br>
2025-09-22 16:05:04 -0300

    feat: trying to implement clarity

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import Clarity from '@microsoft/clarity';
+import Script from "next/script";
 
 export const metadata = {
   title: "Amiko Soluções - Tecnologia Hospitalar Inteligente | SAHRA Assistente Hospitalar",
@@ -63,6 +64,16 @@ export default function RootLayout({
       <link rel="page icon" href="\favicon.ico" />
       {/* biome-ignore lint/style/noUnusedTemplateLiteral: <explanation> */}
 
+      <Script 
+        id="ms-clarity-script"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `(function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "${process.env.NEXT_PUBLIC_CLARITY_ID}");`,
+        }} />
 
         <script type="application/ld+json">
           {JSON.stringify({
